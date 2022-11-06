dc493ac3c1ff97774a22e46f145a7bdc2d0ec6ee
Artem Novik <mr.artnov@mail.ru>
2022-11-06 15:01:46 +0300

    feat: finish task

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -1,8 +1,14 @@
 package lang.print.gaps.task6;
 
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.Logger;
+
 public class FloatTypeCasting {
+    private static final Logger logger = (Logger) LogManager.getLogger();
     public void roundNumber(float numberToBeRounded) {
         int number = (int) numberToBeRounded;
-        System.out.println(number);
+        logger.info(number);
+
     }
 }
